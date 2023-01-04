0829e0185bdb296547520342424fe3a51c398ffb
Mohamed Makhlouf <gaaarw@gmail.com>
2023-01-04 10:22:30 +0200

    create menu by section

---
+++ FoodiSwiftUI/FoodiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiSwiftUIApp.swift
+//  FoodiSwiftUI
+//
+//  Created by Mohamed Makhlouf Ahmed on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
